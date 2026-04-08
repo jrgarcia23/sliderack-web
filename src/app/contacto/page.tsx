7adc0a4ddfacf649592613cfa9078bc61bb77815
JR <sliderack.esnova@gmail.com>
2026-04-08 23:49:16 +0200

    fix: hide red CTA bar on home and contact pages

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -15,7 +15,7 @@ export default function Contacto() {
       <Suspense>
         <ContactPage />
       </Suspense>
-      <Footer />
+      <Footer hideCta />
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <FAQSection />
         <ContactFormSection />
       </main>
-      <Footer />
+      <Footer hideCta />
     </>
   );
 }
